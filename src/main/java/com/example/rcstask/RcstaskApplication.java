b6d73da06c8692ecfc0f32a7ecc28bf6ba0f464c
Leon Galushko <leon-galushko@mail.ru>
2020-09-13 12:32:03 +0300

    Initial commit

---
+++ src/main/java/com/example/rcstask/RcstaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.rcstask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RcstaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RcstaskApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/rcstask/RcstaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.rcstask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RcstaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
